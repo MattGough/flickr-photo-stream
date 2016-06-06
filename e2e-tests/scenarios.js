d367d6c3ab43c6c548885d1d5bcf3df0caaf090b
Matt <mattpgough@gmail.com>
2016-06-06 14:31:38 +0100

    Images now being displayed with photo title.

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -3,11 +3,15 @@
 describe ('flickrApp', function() {
 
   describe ('Photo list', function() {
-    it ('should display a list of 20 photos', function() {
+    it ('should display a list of 20 posts', function() {
       var photoList = element.all(by.repeater('photo in photos'));
 
       browser.get('/');
       expect(photoList.count()).toBe(20);
+    });
+
+    it ('should display the image for each post', function() {
+      expect(element(by.className('image')).isPresent()).toBe(true);
     });
   });
 });
