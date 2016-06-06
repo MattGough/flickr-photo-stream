d3957c32516d794cb5de01691c802341cf0b5f25
Matt <mattpgough@gmail.com>
2016-06-06 14:57:43 +0100

    Made image titles into links to original image on Flickr.

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -13,5 +13,10 @@ describe ('flickrApp', function() {
     it ('should display the image for each post', function() {
       expect(element(by.className('image')).isPresent()).toBe(true);
     });
+
+    it ('should display the title for each post as a link', function() {
+      element(by.className('title')).click();
+      expect(browser.driver.getCurrentUrl()).toMatch('https://www.flickr.com');
+    });
   });
 });
