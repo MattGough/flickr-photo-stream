1a95b4001c608090bcb7606295b2e34ea19d9a25
Matt <mattpgough@gmail.com>
2016-06-06 20:19:26 +0100

    Added basic css and made photo information hidden until image clicked on.

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -5,7 +5,6 @@ describe ('flickrApp', function() {
   describe ('Photo list', function() {
     it ('should display a list of 20 posts', function() {
       var photoList = element.all(by.repeater('photo in photos'));
-
       browser.get('/');
       expect(photoList.count()).toBe(20);
     });
@@ -15,12 +14,14 @@ describe ('flickrApp', function() {
     });
 
     it ('should display the title for each post as a link', function() {
+      element(by.className('image')).click();
       element(by.className('title')).click();
       expect(browser.driver.getCurrentUrl()).toMatch('https://www.flickr.com');
     });
 
     it ('should display the author for each post as a link', function() {
       browser.get('/');
+      element(by.className('image')).click();
       element(by.className('author')).click();
       expect(browser.driver.getCurrentUrl()).toMatch('https://www.flickr.com');
     });
