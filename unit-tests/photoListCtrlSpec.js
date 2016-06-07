1396d13056dbe916c731210844448bab5e7302db
Matt <mattpgough@gmail.com>
2016-06-07 09:40:13 +0100

    Added express server to host Angular host. Used cors-anywhere to run cors proxy when express server is run.

--- unit-tests/photoListCtrlSpec.js
+++ unit-tests/photoListCtrlSpec.js
@@ -8,7 +8,7 @@ describe('PhotoListCtrl', function() {
     $httpBackend = _$httpBackend_;
     var response = { items: [{title: "2016-06-05_06-20-20", author:"nobody@flickr.com (johnsmith)"
 }]}
-    $httpBackend.expectGET('http://localhost:1337/api.flickr.com/services/feeds/photos_public.gne?format=json').respond(response);
+    $httpBackend.expectGET('http://localhost:8080/https://api.flickr.com/services/feeds/photos_public.gne?format=json').respond(response);
 
     scope = $rootScope.$new();
     ctrl = $controller('PhotoListCtrl', {$scope: scope});
