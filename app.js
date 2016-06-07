7652b32f77d85c1b00c764d3e8babf59d9cf44e8
Matt <mattpgough@gmail.com>
2016-06-07 11:14:26 +0100

    Instead of running my own cors proxy (not working on Heroku) I used one already served on heroku.

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/app'));
 app.listen(process.env.PORT || 3000);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
